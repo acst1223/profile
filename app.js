1efa3e960fa74491dcfac2a0130fcba7e96a749e
acst1223 <natnstart@gmail.com>
2023-05-24 00:15:10 +0900

    Fix: init bot

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+// dotenv
+require('dotenv').config();
+
+// discord
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const token = process.env.DISCORD_TOKEN;
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(token);
+
+
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
